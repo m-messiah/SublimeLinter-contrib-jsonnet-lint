d43e010ba8f0cbe7d6031e2531e76d3945cc8870
Maxim Muzafarov <m.muzafarov@gmail.com>
2021-11-18 10:45:26 +0000

    Remove useless colon

--- linter.py
+++ linter.py
@@ -5,7 +5,7 @@ class JSONNetLint(Linter):
     cmd = 'jsonnet-lint ${file_on_disk}'
     tempfile_suffix = '-'
     name = 'jsonnet-lint'
-    regex = r'(?P<filename>.*):(?P<line>[1-9][0-9]*):(?P<col>[1-9][0-9]*)-(?P<end_col>[1-9][0-9]*): (?P<message>.*)'
+    regex = r'(?P<filename>.*):(?P<line>[1-9][0-9]*):(?P<col>[1-9][0-9]*)-(?P<end_col>[1-9][0-9]*) (?P<message>.*)'
     error_stream = STREAM_STDERR
     multiline = False
     defaults = {
