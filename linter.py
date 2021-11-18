596526984047eedfb6fa467c64c4b72c34b9da6f
Maxim Muzafarov <m.muzafarov@gmail.com>
2021-11-18 10:41:26 +0000

    Use STDERR and file_only mode

--- linter.py
+++ linter.py
@@ -1,10 +1,12 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter, STREAM_STDERR
 
 
 class JSONNetLint(Linter):
     cmd = 'jsonnet-lint ${file_on_disk}'
+    tempfile_suffix = '-'
     name = 'jsonnet-lint'
     regex = r'(?P<filename>.*):(?P<line>[1-9][0-9]*):(?P<col>[1-9][0-9]*)-(?P<end_col>[1-9][0-9]*): (?P<message>.*)'
+    error_stream = STREAM_STDERR
     multiline = False
     defaults = {
         'selector': 'source.jsonnet'
